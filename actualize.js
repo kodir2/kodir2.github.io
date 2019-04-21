306664193e3786818210c3c0c44487c8f94f59df
partnercoll <47387993+partnercoll@users.noreply.github.com>
2019-04-21 11:19:45 +0300

    typo

--- actualize.js
+++ actualize.js
@@ -1,6 +1,6 @@
 !function(){
 var old = 'https://api.delivembed.cc';
-var actual = 'https://appi.delivembed.cc/';
+var actual = 'https://appi.delivembed.cc';
 fetch(old+'/ping/').then(r=>r.json()).then(r=>{
 	if(r.status!=='ok')throw new Error('1');
 }).catch(replace);
