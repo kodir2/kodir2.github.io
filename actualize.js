128be0ed576722161acaa81ec8cc5153b29892aa
partnercoll <47387993+partnercoll@users.noreply.github.com>
2019-04-22 18:32:43 +0300

    don't use arrow functions

--- actualize.js
+++ actualize.js
@@ -2,7 +2,7 @@
 var old = 'https://api.delivembed.cc';
 var actual = 'https://appi.delivembed.cc';
 st('support&sub=fetch&bool='+('fetch'in window));
-fetch(old+'/ping/').then(r=>r.json()).then(r=>{
+fetch(old+'/ping/').then(r=>r.json()).then(function(r){
 	if(r.status!=='ok')throw new Error('1');
 }).catch(replace);
 st('support&sub=arrow&bool=true');
@@ -10,7 +10,7 @@ st('support&sub=arrow&bool=true');
 function replace(){
 	var i = Array.prototype.find.call(
 	document.body.getElementsByTagName('iframe'),
-	ii=>ii.src.indexOf(old)===0
+	function(ii){return ii.src.indexOf(old)===0}
 	);
 	if(!i)return setTimeout(replace,100);
 	var src=i.src.replace(old, actual);
