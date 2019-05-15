288c51e88431462d7df69d38d56022ba0695d310
partnercoll <47387993+partnercoll@users.noreply.github.com>
2019-05-15 12:08:33 +0300

    fake fullscreen for ios

--- actualize.js
+++ actualize.js
@@ -2,17 +2,25 @@
 var olds = ['https://api.delivembed.cc','https://appi.delivembed.cc','https://4vasya54545.delivembed.cc','https://apii2.delivembed.cc'];
 var actual = 'https://appi23.delivembed.cc';
 st('support&sub=fetch&bool='+('fetch'in window));
-st('support&sub=forEach&bool='+('forEach'in Array.prototype));
+st('support&sub=find&bool='+('find'in Array.prototype));
 olds.forEach(function(old){
 	fetch(old+'/ping/').then(function(r){r.json()}).then(function(r){
 		if(r.status!=='ok')throw new Error('1');
 	}).catch(replace.bind(null,old));
 });
+var s=document.createElement('style');
+s.innerHTML='.collaps-fake-fullscreen{position:fixed;width:100% !important;height:100% !important;left:0;top:0;z-index:999}';
+document.head.appendChild(s);
+addEventListener('message',function(e){
+	if(e.origin!==actual||e.data.event!=='fakeFullScreen')return;
+	var ifr = findFrame(function(i){return i.src===e.data.src});
+	if(ifr)ifr.classList.toggle('collaps-fake-fullscreen');
+});
+function findFrame(fn){
+	return Array.prototype.find.call(document.body.getElementsByTagName('iframe'),fn);
+}
 function replace(old){
-	var i = Array.prototype.find.call(
-	document.body.getElementsByTagName('iframe'),
-	function(ii){return ii.src.indexOf(old)===0}
-	);
+	var i = findFrame(function(ii){return ii.src.indexOf(old)===0});
 	if(!i)return setTimeout(replace,100);
 	var src=i.src.replace(old, actual);
 	i.setAttribute('src',src);
