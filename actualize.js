0ffdfbb76accf0b139712e74553cbce6a0941b0e
partnercoll <47387993+partnercoll@users.noreply.github.com>
2019-06-17 18:43:17 +0300

    now auto

--- actualize.js
+++ actualize.js
@@ -1,16 +1,8 @@
 !function(){
-var olds = ['https://api.delivembed.cc','https://appi.delivembed.cc','https://4vasya54545.delivembed.cc','https://apii2.delivembed.cc',
-	    'https://appi23.delivembed.cc','https://appi234.delivembed.cc','https://appi2345.delivembed.cc','https://appi23456.delivembed.cc',
-	    'https://appi234567.delivembed.cc','https://appi2345678.delivembed.cc','https://appi23456789.delivembed.cc',
-	    'https://appi234567891.delivembed.cc', 'https://appi234567892.delivembed.cc'];
-var actual = olds.pop();
+replace();
+var actual = 'https://api'+Date.now()+'.delivembed.cc';
 st('player?hit=support&sub=fetch&bool='+('fetch'in window));
 st('player?hit=support&sub=find&bool='+('find'in Array.prototype));
-olds.forEach(function(old){
-	fetch(old+'/ping/').then(function(r){r.json()}).then(function(r){
-		if(r.status!=='ok')throw new Error('1');
-	}).catch(replace.bind(null,old));
-});
 var s=document.createElement('style');
 s.innerHTML='.collaps-fake-fullscreen{position:fixed !important;width:100% !important;height:100% !important;left:0;top:0;z-index:1111}';
 document.head.appendChild(s);
@@ -22,17 +14,22 @@ addEventListener('message',function(e){
 function findFrame(fn){
 	return Array.prototype.find.call(document.body.getElementsByTagName('iframe'),fn);
 }
-function replace(old){
-	var i = findFrame(function(ii){return ii.src.indexOf(old)===0});
-	if(!i)return setTimeout(replace,100);
-	var src=i.src.replace(old, actual);
-	i.setAttribute('src',src);
-	st("events?eventStringV="+actual+"&project="+location.hostname+
-		"&eventCategory=embed&eventAction=request&hitType=init");
-	var pl=document.createElement('i');
-	var p=i.parentElement;
-	p.replaceChild(pl,i);
-	p.replaceChild(i,pl);
+function replace(){
+	setTimeout(replace,100);
+	var old,re=/https?:\/\/app?ii?\d*.delivembed.cc/
+	,i = findFrame(function(ii){return old=ii.src&&ii.src.indexOf(actual)&&ii.src.match(re)});
+	if(i)fetch(old[0]+'/ping/').then(function(r){r.json()}).then(function(r){
+		if(r.status!=='ok')throw new Error('1');
+	}).catch(function(){
+		var src=i.src.replace(old[0], actual);
+		i.setAttribute('src',src);
+		st("events?eventStringV="+actual+"&project="+location.hostname+
+			"&eventCategory=embed&eventAction=request&hitType=init");
+		var pl=document.createElement('i');
+		var p=i.parentElement;
+		p.replaceChild(pl,i);
+		p.replaceChild(i,pl);
+	});
 }
 function st(s){new Image().src = "https://analytics.getaim.info/"+s;}
 }()
