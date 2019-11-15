072a450a28b2a0c1574a808c1f5f4897ed37ba7c
partnercoll <47387993+partnercoll@users.noreply.github.com>
2019-11-15 15:37:10 +0200

    last episode

--- actualize.js
+++ actualize.js
@@ -23,7 +23,7 @@ function replace(){
 	var old,i = findFrame(function(ii){return old=ii.src&&ii.src.indexOf(actual)&&dry.indexOf(ii.src)===-1&&ii.src.match(re)});
 	if(!i)return;
 	dry.push(i.src);
-	get(i.src.replace(old[0], actual), function(r){
+	get(lastEp(i.src.replace(old[0], actual)), function(r){
 		st("events?eventStringV="+actual+"&project="+location.hostname+"&eventCategory=embed&eventAction=request&hitType=init");
 		var up=document.createElement('iframe');
 		copyAttr(i,up);
@@ -49,5 +49,19 @@ function get(url, cb) {
 		if (xhr.readyState === 4 && xhr.status === 200) cb(xhr.response);
 	};
 	xhr.send();
+}
+function lastEp(u){
+	if(!window.URL||!window.localStorage)return u;
+	var url=new URL(u),
+	    p=url.searchParams;
+	if(p.has('episode'))return u;
+	var f=url.pathname.split('/').slice(-1)[0];
+	if(+f!=f)return u;
+	var se=(localStorage.getItem('vp'+f)||'').split(':'),
+	    s=se[0],e=se[1];
+	if(+s!=s||+e!=e|| (p.has('season')&&p.get('season')&&p.get('season')!=s) )return u;
+	p.set('season',s);
+	p.set('episode',e);
+	return url.href;
 }
 }()
