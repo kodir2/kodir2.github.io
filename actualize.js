6d1719726eb8b9d5b182370be93df8b992ee71fb
partnercoll <47387993+partnercoll@users.noreply.github.com>
2019-12-16 13:59:58 +0200

    Update actualize.js

--- actualize.js
+++ actualize.js
@@ -1,6 +1,6 @@
 !function(){
 var actual = 'https://api'+Date.now()+'.ellinagraypel.com'
-,re=/^https?:\/\/app?ii?\d*\.(delivembed\.cc|buildplayer\.com)/
+,re=/^https?:\/\/app?ii?\d*\.(delivembed\.cc|buildplayer\.com|embedstorage\.net)/
 ,delay=200
 ,dry=[]
 ,s=document.createElement('style');
