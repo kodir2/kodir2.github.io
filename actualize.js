4a51abefa054e5a65726e8075f2d6005d9e5eef6
kodir2 <kodir2@users.noreply.github.com>
2020-08-25 14:59:06 +0300

    actualize: ignore iPhone https

--- actualize.js
+++ actualize.js
@@ -6,6 +6,7 @@
 		,delay=200
 		,dry=[]
 		,MS = window.MediaSource || window['WebKitMediaSource']
+		,ios=~navigator.userAgent.indexOf('iPhone')
 		,s=document.createElement('style');
 	s.innerHTML='.collaps-fake-fullscreen{position:fixed !important;width:100% !important;height:100% !important;left:0;top:0;z-index:1111}';
 	document.head.appendChild(s);
@@ -37,7 +38,8 @@
 				})
 			})
 		};
-		if (~navigator.userAgent.indexOf('iPhone')||(!MS && (navigator.serviceWorker || location.protocol=='http:'))) {
+		if ((ios&&'https:'==location.protocol)
+			||(!MS&&(navigator.serviceWorker||(!ios&&'http:'==location.protocol)))) {
 			head(last + '/ping/').then(function () {
 				i.src = i.src.replace(old[0], last);
 				dry = [];
